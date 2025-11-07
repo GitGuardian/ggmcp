dcb05fd8ce91c865ad1bbd9035dd8bef94fc43b6
Kevin Nedelec <kevin.nedelec@gitguardian.com>
2025-11-07 10:42:14 +0100

    chore(run_http_server): take default env vars

--- scripts/run_http_server.py
+++ scripts/run_http_server.py
@@ -2,10 +2,10 @@
 import os
 
 # Set environment variables before importing
-os.environ["GITGUARDIAN_URL"] = "http://127.0.0.1:3000"
-os.environ["ENABLE_LOCAL_OAUTH"] = "false"
-os.environ["MCP_PORT"] = "8088"
-os.environ["MCP_HOST"] = "127.0.0.1"
+os.environ["GITGUARDIAN_URL"] = os.environ.get("GITGUARDIAN_URL", "http://127.0.0.1:3000")
+os.environ["ENABLE_LOCAL_OAUTH"] = os.environ.get("ENABLE_LOCAL_OAUTH", "false")
+os.environ["MCP_PORT"] = os.environ.get("MCP_PORT", "8088")
+os.environ["MCP_HOST"] = os.environ.get("MCP_HOST", "127.0.0.1")
 
 if __name__ == "__main__":
     # For SecOps server:
